00d50b5841816e5fbcfc369ef19722501fb535c3
Abhishek Bhardwaj <abhishek.purdue@gmail.com>
2022-09-15 10:00:48 -0700

    pushing code for first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import github from "./db.js";
+import githubQuery from "./Query.js";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
+  let [userName, setUserName] = useState("");
+  let [repoList, setRepoList] = useState(null);
+
+  // const fetchData = useCallback(() => {
+  //   fetch(github.baseUrl, {
+  //     method: "POST",
+  //     headers: github.headers,
+  //     body: JSON.stringify(githubQuery),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setUserName(data.data.viewer.name);
+  //       console.log(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    fetch(github.baseUrl, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(githubQuery),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const viewer = data.data.viewer;
+        setUserName(viewer.name);
+        setRepoList(viewer.repositories.nodes);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const Basic = (props) => {
+    return (
+      <div>
+        <p>Hello World {props.name}</p>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5">
+      <Basic name="Abhishek"/>
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>Repos
+      </h1>
+      <h4>Hello {userName} !!</h4>
+      {repoList && (
+        <ul className="list-group list-group-flush">
+          {repoList.map((repo, i) => {
+            return (
+              <li className="list-group-item" key={repo.id}>
+                <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                  {repo.name}
+                </a>
+                <p className="small">{repo.description}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
