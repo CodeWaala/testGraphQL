d6a341f1dffa4f819e89e29e6591028ab75da08f
Abhishek Bhardwaj <abhishek.purdue@gmail.com>
2022-09-15 10:14:19 -0700

    adding db.js

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseUrl: "https://api.github.com/graphql",
+    username: "codewaala",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer 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"
+    },
+}
+
+export default github;
